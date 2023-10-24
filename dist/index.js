6e27eb9b81d39c759c5f350ff67497345c788508
Joe Pea <joe@trusktr.io>
2023-10-24 01:22:23 -0700

    v0.10.1

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,5 @@ export * from './variable.js';
 export * from './attribute.js';
 export * from './element.js';
 export * from './css.js';
-export const version = '0.10.0';
+export const version = '0.10.1';
 //# sourceMappingURL=index.js.map
