302bac6139a7ba21d9bc8e112b7e14988055f72b
Joe Pea <joe@trusktr.io>
2023-11-17 14:30:15 -0800

    v0.11.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,5 @@ export * from './attribute.js';
 export * from './element.js';
 export * from './css.js';
 export type { JSX } from './jsx-runtime.js';
-export declare const version = "0.10.1";
+export declare const version = "0.11.0";
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export * from './element.js'
 export * from './css.js'
 export type {JSX} from './jsx-runtime.js'
 
-export const version = '0.10.1'
+export const version = '0.11.0'
