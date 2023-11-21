739e91af5867d74dfb393b37281f667232a9f2cb
Joe Pea <joe@trusktr.io>
2023-11-21 00:47:18 -0800

    v0.11.2

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,5 @@ export * from './attribute.js';
 export * from './element.js';
 export * from './css.js';
 export type { JSX } from './jsx-runtime.js';
-export declare const version = "0.11.1";
+export declare const version = "0.11.2";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@ export * from './LumeElement.js';
 export * from './attribute.js';
 export * from './element.js';
 export * from './css.js';
-export const version = '0.11.1';
+export const version = '0.11.2';
 //# sourceMappingURL=index.js.map
