31a4387a109ec266efeca84428dd09919353e998
Joe Pea <joe@trusktr.io>
2023-11-20 23:57:18 -0800

    v0.11.1

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@ export * from './LumeElement.js';
 export * from './attribute.js';
 export * from './element.js';
 export * from './css.js';
-export const version = '0.11.0';
+export const version = '0.11.1';
 //# sourceMappingURL=index.js.map
