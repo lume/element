cdb652dc5c5ab294d9a4b1da262860da64d072c5
Joe Pea <joe@trusktr.io>
2024-09-27 01:06:46 -0700

    v0.13.1

--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,5 @@ export * from './attribute.js';
 export * from './element.js';
 export * from './css.js';
 export { identityTemplateTag, camelCaseToDash, dashCaseToCamelCase, } from './utils.js';
-export const version = '0.13.0';
+export const version = '0.13.1';
 //# sourceMappingURL=index.js.map
