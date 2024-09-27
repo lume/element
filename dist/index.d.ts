df0698dc5f984e69b3cdb4e3512bf7b320459abd
Joe Pea <joe@trusktr.io>
2024-09-27 00:45:57 -0700

    v0.13.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,5 +4,5 @@ export * from './element.js';
 export * from './css.js';
 export { identityTemplateTag, camelCaseToDash, dashCaseToCamelCase, type CamelCasedProps, type DashCasedProps, } from './utils.js';
 export type { JSX } from './jsx-runtime.js';
-export declare const version = "0.12.4";
+export declare const version = "0.13.0";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -3,5 +3,5 @@ export * from './attribute.js';
 export * from './element.js';
 export * from './css.js';
 export { identityTemplateTag, camelCaseToDash, dashCaseToCamelCase, } from './utils.js';
-export const version = '0.12.4';
+export const version = '0.13.0';
 //# sourceMappingURL=index.js.map
