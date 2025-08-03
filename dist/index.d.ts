e54be584d47a4e901d2a2373de1b503d6a5e9ab8
Joe Pea <joe@trusktr.io>
2025-08-02 20:18:57 -0700

    v0.16.1

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -3,5 +3,5 @@ export * from './decorators/index.js';
 export * from './css.js';
 export { identityTemplateTag, camelCaseToDash, dashCaseToCamelCase, type CamelCasedProps, type DashCasedProps, } from './utils.js';
 export type { JSX } from './jsx-runtime.js';
-export declare const version = "0.16.1-beta.0";
+export declare const version = "0.16.1";
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@ export * from './LumeElement.js';
 export * from './decorators/index.js';
 export * from './css.js';
 export { identityTemplateTag, camelCaseToDash, dashCaseToCamelCase, } from './utils.js';
-export const version = '0.16.1-beta.0';
+export const version = '0.16.1';
 //# sourceMappingURL=index.js.map
