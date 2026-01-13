0c72197888dcf0654894b74f2983ae03393ed73a
Joe Pea <joe@lume.io>
2026-01-12 16:05:06 -0800

    v0.17.0

--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@ export * from './LumeElement.js';
 export * from './decorators/index.js';
 export * from './css.js';
 export { identityTemplateTag, camelCaseToDash, dashCaseToCamelCase, } from './utils.js';
-export const version = '0.16.2';
+export const version = '0.17.0';
 //# sourceMappingURL=index.js.map
